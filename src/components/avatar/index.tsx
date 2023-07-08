61fc529dc5cedb8787ee4a7888099ccca2df2b1b
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-08 19:44:21 +1000

    test images, css mostly done

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -6,8 +6,8 @@ export default function Avatar(){
         <section id="avatar" className="flex container justify-center  rounded-md hover:bg-gray-200 cursor-pointer p-2 text-gray-700 hover:text-blue-400">
             <div className="m-5 p-5 text-center">
                 <Image
-                    className="mx-auto"
-                    src="next.svg"
+                    className="mx-auto rounded-lg"
+                    src="/angry.jpg"
                     width={0}
                     height={0}
                     sizes="100vw"
