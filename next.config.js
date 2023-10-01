cd34a78e61e4ed6a063168cac2d637102f363a94
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-10-01 17:45:36 +1100

    build reattempt

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/v1:path*',
-                destination: `${process.env.URL}/api/v1:path*`,
+                destination: `${process.env.URL}/:path*`,
             }
         ]
     }
